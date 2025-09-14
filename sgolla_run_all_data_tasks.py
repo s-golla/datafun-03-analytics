f8606fd79b2e56b91dbe36d3bc4e92724e875687
s-golla <saratchandragolla@gmail.com>
2025-09-14 00:00:44 -0500

    uploading logs, get_data and processed_data

--- sgolla_run_all_data_tasks.py
+++ sgolla_run_all_data_tasks.py
@@ -8,10 +8,10 @@ from sgolla_get_csv import main as get_csv_main
 from sgolla_get_excel import main as get_excel_main
 from sgolla_get_json import main as get_json_main
 from sgolla_get_text import main as get_text_main
-from sgolla_process_csv import main as process_csv_main
-from sgolla_process_excel import main as process_excel_main
-from sgolla_process_json import main as process_json_main
-from sgolla_process_text import main as process_text_main
+from sgolla_process_csv import process_csv_file
+from sgolla_process_excel import process_excel_file
+from sgolla_process_json import process_json_file
+from sgolla_process_text import process_text_file
 
 
 def run_all():
@@ -22,10 +22,10 @@ def run_all():
     get_text_main()
 
     print("\nRunning all 'process' functions...")
-    process_csv_main()
-    process_excel_main()
-    process_json_main()
-    process_text_main()
+    process_csv_file()
+    process_excel_file()
+    process_json_file()
+    process_text_file()
 
 
 if __name__ == "__main__":
